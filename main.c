199bd9349adbe7e3e6b02a62b5f625789608fe0c
lnb1421 <92702100+lnb1421@users.noreply.github.com>
2021-11-28 21:50:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "misFunciones.h"
-
+#define CLS "CLS"
+#define PAUSE "PAUSE"
 
 
 
@@ -13,7 +14,7 @@ int main()
     int resultadoSuma = 0;
     int resultadoResta = 0;
     int resultadoMultiplicacion = 0;
-    int resultadoDivision = 0;
+    float resultadoDivision = 0;
     int resultadoFactorial = 0;
     int sw, valA, valB;
     char numeroA[50];
@@ -38,20 +39,16 @@ int main()
         system(CLS);
 
 
-
-        if(seleccion == 7){
+        if(seleccion == 7)
+        {
 
             break;
         }
 
-        system(CLS);
-
-
-
         printf("ingrese un valor : ");
         scanf("%d", &A);
 
-        printf("ingrese un segundo valor : ");
+        printf("ingrese el segundo valor : ");
         scanf("%d", &B);
 
 
@@ -59,56 +56,54 @@ int main()
         //validacionNumA(numeroA, A);
         //validacionNumB(numeroB, B);
 
+
         switch(seleccion)
         {
 
         case 1 :
-            resultadoSuma = suma(A, B, total);
+            suma(A, B, total);
 
-            printf("%d + %d = %d \n", A, B, resultadoSuma);
             system(PAUSE);
             break;
 
         case 2 :
-            resultadoResta = resta(A, B, total);
+            resta(A, B, total);
 
-            printf("%d - %d = %d \n", A, B, resultadoResta);
+
             system(PAUSE);
             break;
 
         case 3 :
-            resultadoMultiplicacion = multiplicacion(A, B, total);
+            multiplicacion(A, B, total);
 
-            printf("%d x %d = %d \n", A, B, resultadoMultiplicacion);
+
             system(PAUSE);
             break;
 
 
 
         case 4 :
-            resultadoDivision = division(A, B, total);
+            division( A, B,total);
 
-            printf("%d / %d = %.2f \n", A, B, resultadoDivision);
             system(PAUSE);
             break;
 
 
         case 5 :
-            resultadoFactorial = factorial(A, valor);
+            factorial(A, valor);
 
-            printf("!F = %d \n", resultadoFactorial);
+
             system(PAUSE);
             break;
 
 
         case 6 :
-            resultadoSuma = suma(A, B, total);
-            resultadoResta = resta(A, B, total);
-            resultadoMultiplicacion = multiplicacion(A, B, total);
-            resultadoDivision = division(A, B, total);
-            resultadoFactorial = factorial(A, valor);
+            suma(A, B, total);
+            resta(A, B, total);
+            multiplicacion(A, B, total);
+            division(A, B, total);
+            factorial(A, valor);
 
-            mostrarResultados(A, B, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision, resultadoFactorial);
             system(PAUSE);
             break;
 
